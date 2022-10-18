9ddf539e50ff01c2dba39257b643bae46aab5fd5
Stephen Shead <18731093+sshead@users.noreply.github.com>
2022-10-19 08:30:24 +1100

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -22,6 +22,7 @@ fi
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+  echo '# Set PATH, MANPATH, etc., for Homebrew.' >> $HOME/.zprofile
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
