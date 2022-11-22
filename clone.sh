84e644e0c010fa1bcc9235b7638e19268587e690
Stephen Shead <18731093+sshead@users.noreply.github.com>
2022-11-22 12:42:24 +1100

    wip

--- clone.sh
+++ clone.sh
@@ -14,20 +14,22 @@ fi
 echo "Cloning repositories..."
 
 SITES=$HOME/Sites
+CATDIR=$SITES/cat
+PRAYERDIR=$SITES/prayer
 
 # CAT Admin
-git clone git@bitbucket.org:aussieinchile/catadmin.git $SITES/catadmin/catadmin
-git clone git@bitbucket.org:aussieinchile/catadmin.git/wiki $SITES/catadmin/wiki
+git clone git@bitbucket.org:aussieinchile/catadmin.git $CATDIR/catadmin
+git clone git@bitbucket.org:aussieinchile/catadmin.git/wiki $CATDIR/wiki
 
 # Prayer
-git clone git@github.com:sshead/pray-517-vuejs.git $SITES/prayer/pray-517-vuejs
-git clone git@github.com:sshead/pray-517-tall.git $SITES/prayer/pray-517-tall
-git clone git@github.com:sshead/pray-517-laravel-7.git $SITES/prayer/pray-517-laravel-7
-git clone git@github.com:sshead/prayer-vigil-517.git $SITES/prayer/prayer-vigil-517
+git clone git@github.com:sshead/pray-517-vuejs.git $PRAYERDIR/pray-517-vuejs
+git clone git@github.com:sshead/pray-517-tall.git $PRAYERDIR/pray-517-tall
+git clone git@github.com:sshead/pray-517-laravel-7.git $PRAYERDIR/pray-517-laravel-7
+git clone git@github.com:sshead/prayer-vigil-517.git $PRAYERDIR/prayer-vigil-517
 
 # Personal
 git clone git@github.com:sshead/dogfood-ratings.git $SITES/dogfood-ratings
 
 echo "Parking Sites directory with Valet..."
-valet park $SITES/prayer
-valet park $SITES/catadmin
+valet park $PRAYERDIR
+valet park $CATDIR
